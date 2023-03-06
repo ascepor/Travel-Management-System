e2d8258b621a246f42debd244b2a9a1f1f9239f4
ascepor <118761959+ascepor@users.noreply.github.com>
2023-03-06 19:31:22 +0530

    Add files via upload

---
+++ src/MyConnection.java
@@ -0,0 +1,46 @@
+
+import java.sql.*;
+import java.util.*;
+import java.io.*;
+
+public class MyConnection 
+{
+	// static Connection con=null;
+	static Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/sonoo","root","root");  
+
+	public static Connection getConnection(String path)
+	{
+		try
+		{
+			if(con==null)
+			{
+				FileInputStream fis=new FileInputStream(path);
+
+				Properties p=new Properties();
+				p.load(fis);
+
+				String driver=p.getProperty("driver");
+				String url=p.getProperty("url");
+				String username=p.getProperty("username");
+				String password=p.getProperty("password");
+
+				Class.forName(driver);
+	
+				con=DriverManager.getConnection(url,username,password);
+			}
+			return con;
+		}
+		catch (SQLException se)
+		{
+			se.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+
+		}
+		
+		return con;
+	}
+}
